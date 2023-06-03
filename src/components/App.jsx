2d97cd28d43151c5e8ae663c573ba70196e258d5
VasylRybotytskyi <113139368+VasylRybotytskyi@users.noreply.github.com>
2023-06-03 21:10:16 +0300

    added login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
 import { Layout } from './Layout/Layout';
-import { Home } from 'pages/Home/Home';
+import Home from 'pages/Home/Home';
 import { GlobalStyle } from './GlobalStyle';
 
 export const App = () => {
