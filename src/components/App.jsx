1b9eba33005a6a927582b573f0b02e6d55828d92
VasylRybotytskyi <113139368+VasylRybotytskyi@users.noreply.github.com>
2023-06-03 15:28:19 +0300

    added Route in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,18 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
 import { Layout } from './Layout/Layout';
 import { Home } from 'pages/Home/Home';
+import { GlobalStyle } from './GlobalStyle';
 export const App = () => {
   return (
     <>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />}></Route>
+          <Route path="/contacts"></Route>
+          <Route path="/login"></Route>
+          <Route path="/register"></Route>
+          <Route path="*" element={<Navigate to="/" />} />
         </Route>
       </Routes>
     </>
