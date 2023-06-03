7e579e2e26dc62b362fd0693d4c7ac3d4ee422ea
VasylRybotytskyi <113139368+VasylRybotytskyi@users.noreply.github.com>
2023-06-03 20:15:15 +0300

    update store

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,1 +1,46 @@
-export const AppBar = () => {};
+import { Spiner } from 'pages/ContactList/ContactList.styled';
+import { UserMenu } from 'components/UserMenu/UserMenu';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+export const AppBar = () => {
+  const { isLoaggedIn, isLoading } = useSelector(state => state.auth);
+  return (
+    <header>
+      {isLoading && <Spiner />}
+
+      <div>
+        <nav>
+          <div>
+            <Link to="/">
+              {/* <HomeIcon /> */}
+              Home
+            </Link>
+            {isLoaggedIn && (
+              <Link to="/contacts">
+                {/* <ContactIcon /> */}
+                Contacts
+              </Link>
+            )}
+          </div>
+          <div>
+            {isLoaggedIn ? (
+              <UserMenu />
+            ) : (
+              <>
+                <Link to="/register">
+                  {/* <RegIcon /> */}
+                  Register
+                </Link>
+                <Link to="/login">
+                  {/* <LogInIcon /> */}
+                  Log in
+                </Link>
+              </>
+            )}
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
