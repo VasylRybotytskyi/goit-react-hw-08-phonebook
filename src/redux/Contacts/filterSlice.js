2a33ab7e0736bbd498f18ad50c0b301e0427865c
VasylRybotytskyi <113139368+VasylRybotytskyi@users.noreply.github.com>
2023-06-05 18:37:27 +0300

    update operations

--- src/redux/Contacts/filterSlice.js
+++ src/redux/Contacts/filterSlice.js
@@ -1,20 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { logOut } from 'redux/Authorization/operations';
 
+// Створення slice (зрізу) з використанням createSlice з Redux Toolkit
 export const filterSlice = createSlice({
-  name: 'filter',
-  initialState: '',
+  name: 'filter', // Назва зрізу
+  initialState: '', // Початковий стан для filter
   reducers: {
     filtration: (state, action) => {
-      return (state = action.payload);
+      return (state = action.payload); // Редуктор, який змінює значення filter на значення з action.payload
     },
   },
   extraReducers: builder => {
     builder.addCase(logOut.fulfilled, state => {
-      return (state = '');
+      return (state = ''); // Редуктор, який очищує значення filter при виконанні logOut
     });
   },
 });
 
-export const { filtration } = filterSlice.actions;
-export const filterReducer = filterSlice.reducer;
+export const { filtration } = filterSlice.actions; // Експорт action creators зі slice
+export const filterReducer = filterSlice.reducer; // Експорт редюсера filterReducer
--- src/redux/Contacts/operations.js
+++ src/redux/Contacts/operations.js
@@ -1,55 +1,60 @@
-import axios from 'axios';
-import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios'; // Імпорт модуля axios для виконання HTTP-запитів
+import { createAsyncThunk } from '@reduxjs/toolkit'; // Імпорт функції createAsyncThunk з Redux Toolkit
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com/'; // Встановлення базового URL для HTTP-запитів
 
+// Створення асинхронної thunk для отримання контактів
 export const fetchContacts = createAsyncThunk(
-  'contacts/fetchContacts',
+  'contacts/fetchContacts', // Назва дії (action)
   async (_, { rejectWithValue }) => {
     try {
-      const response = await axios.get('/contacts');
-      return response.data;
+      const response = await axios.get('/contacts'); // Виконання GET-запиту до /contacts
+      return response.data; // Повернення отриманих даних
     } catch (e) {
-      return rejectWithValue(e.message);
+      return rejectWithValue(e.message); // Повернення помилки з значенням для відхилення з помилкою
     }
   }
 );
 
+// Створення асинхронної thunk для видалення контакту
 export const deleteContact = createAsyncThunk(
-  'contacts/deleteContact',
+  'contacts/deleteContact', // Назва дії (action)
   async (id, { rejectWithValue }) => {
     try {
-      const response = await axios.delete(`/contacts/${id}`);
-      return response.data;
+      const response = await axios.delete(`/contacts/${id}`); // Виконання DELETE-запиту до /contacts/{id}
+      return response.data; // Повернення отриманих даних
     } catch (e) {
-      return rejectWithValue(e.message);
+      return rejectWithValue(e.message); // Повернення помилки з значенням для відхилення з помилкою
     }
   }
 );
 
+// Створення асинхронної thunk для додавання контакту
 export const addContact = createAsyncThunk(
-  'contacts/addContact',
+  'contacts/addContact', // Назва дії (action)
   async (subscriber, { rejectWithValue }) => {
     try {
-      const response = await axios.post(`/contacts`, subscriber);
-      return response.data;
+      const response = await axios.post(`/contacts`, subscriber); // Виконання POST-запиту до /contacts з переданим об'єктом контакту
+      return response.data; // Повернення отриманих даних
     } catch (e) {
-      return rejectWithValue(e.message);
+      return rejectWithValue(e.message); // Повернення помилки з значенням для відхилення з помилкою
     }
   }
 );
 
+// Створення асинхронної thunk для редагування контакту
 export const redactContatc = createAsyncThunk(
-  'contacts/redactContatc',
+  'contacts/redactContatc', // Назва дії (action)
   async (data, { rejectWithValue }) => {
     try {
       const response = await axios.patch(`/contacts/${data.id}`, {
+        // Виконання PATCH-запиту до /contacts/{id} з переданими полями для оновлення
         name: data.name,
         number: data.number,
       });
-      return response.data;
+      return response.data; // Повернення отриманих даних
     } catch (e) {
-      return rejectWithValue(e.message);
+      return rejectWithValue(e.message); // Повернення помилки з значенням для відхилення з помилкою
     }
   }
 );
