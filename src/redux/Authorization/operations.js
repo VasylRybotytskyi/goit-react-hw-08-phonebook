08edf267e9aa9356da98ef4a96c86ce3d81cd601
VasylRybotytskyi <113139368+VasylRybotytskyi@users.noreply.github.com>
2023-06-05 18:48:55 +0300

    update operations

--- src/redux/Authorization/operations.js
+++ src/redux/Authorization/operations.js
@@ -1,18 +1,22 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
-const setAuthHeader = token =>
-  (axios.defaults.headers.common.Authorization = `Bearer ${token}`);
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com/'; // Встановлення базового URL для запитів axios
+
+const setAuthHeader = (
+  token // Функція для встановлення заголовка авторизації з токеном
+) => (axios.defaults.headers.common.Authorization = `Bearer ${token}`);
+
 const cleanAuthHeader = () =>
+  // Функція для очищення заголовка авторизації
   (axios.defaults.headers.common.Authorization = '');
 
 export const register = createAsyncThunk(
   'auth/register',
   async (user, { rejectWithValue }) => {
     try {
-      const response = await axios.post('/users/signup', user);
-      setAuthHeader(response.data.token);
+      const response = await axios.post('/users/signup', user); // Виконання POST-запиту на створення нового користувача
+      setAuthHeader(response.data.token); // Встановлення заголовка авторизації з отриманим токеном
       return response.data;
     } catch (e) {
       return rejectWithValue(e.message);
@@ -24,8 +28,8 @@ export const loginization = createAsyncThunk(
   'auth/loginization',
   async (user, { rejectWithValue }) => {
     try {
-      const response = await axios.post('/users/login', user);
-      setAuthHeader(response.data.token);
+      const response = await axios.post('/users/login', user); // Виконання POST-запиту на вхід користувача
+      setAuthHeader(response.data.token); // Встановлення заголовка авторизації з отриманим токеном
       return response.data;
     } catch (e) {
       return rejectWithValue(e.message);
@@ -37,8 +41,8 @@ export const logOut = createAsyncThunk(
   'auth/logout',
   async (user, { rejectWithValue }) => {
     try {
-      await axios.post('/users/logout', user);
-      cleanAuthHeader();
+      await axios.post('/users/logout', user); // Виконання POST-запиту на вихід користувача
+      cleanAuthHeader(); // Очищення заголовка авторизації
     } catch (e) {
       return rejectWithValue(e.message);
     }
@@ -48,11 +52,11 @@ export const logOut = createAsyncThunk(
 export const refreshUser = createAsyncThunk(
   'auth/refreshUser',
   async (_, { getState, rejectWithValue }) => {
-    const { token } = getState().auth;
-    token && setAuthHeader(token);
+    const { token } = getState().auth; // Отримання токена зі стану
+    token && setAuthHeader(token); // Встановлення заголовка авторизації, якщо токен існує
 
     try {
-      const response = await axios.get('/users/current');
+      const response = await axios.get('/users/current'); // Виконання GET-запиту на отримання поточного користувача
       return response.data;
     } catch (e) {
       return rejectWithValue(e.message);
